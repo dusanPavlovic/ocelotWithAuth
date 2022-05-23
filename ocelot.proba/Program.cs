f683d334b90b0ea94a2aaa60a0184a3bc324df55
Dusan Pavlovic <dusan.pavlovic@enlightit.rs>
2022-05-23 11:04:41 +0200

    gateway the first version

--- ocelot.proba/Program.cs
+++ ocelot.proba/Program.cs
@@ -1,10 +1,12 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace API_Gateway
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -15,8 +17,12 @@ namespace API_Gateway
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
                     webBuilder.UseStartup<Startup>();
-                }).ConfigureLogging( logging => logging.AddConsole());
+                    webBuilder.ConfigureAppConfiguration(config =>
+                        config.AddJsonFile($"ocelot.{env}.json"));
+                })
+                .ConfigureLogging( logging => logging.AddConsole());
             
 
     }
